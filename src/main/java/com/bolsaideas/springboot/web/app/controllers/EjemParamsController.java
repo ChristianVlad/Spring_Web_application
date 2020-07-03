2b0a81deec480e160e59f0282855c6eb80ccced5
ChristianVlad <Christian.valladarezz@gmail.com>
2020-07-03 16:00:24 -0600

    a lot params

--- src/main/java/com/bolsaideas/springboot/web/app/controllers/EjemParamsController.java
+++ src/main/java/com/bolsaideas/springboot/web/app/controllers/EjemParamsController.java
@@ -18,8 +18,13 @@ public class EjemParamsController {
 	@GetMapping("/string")
 	public String param(@RequestParam(name="texto") String texto, Model model) {
 		model.addAttribute("resultado", "El texto enviado es: " + texto);
-		return "params/ver";
-		 
-		
+		return "params/ver";	 		
+	}
+
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el numero es '" + numero + "'" );
+		return "params/ver";	 
 	}
 }
